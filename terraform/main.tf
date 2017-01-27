efde3691eb7037d5792c7f53c9ef9beb6e79ee20
Mathew Branyon <mbranyon@zvelo.com>
2017-01-27 10:24:16 -0700

    clearing old values

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 # Generic AWS variables we need
 #   create cluster with kops first, then fill these out
 variable "availabilityZone"             { default = "us-east-1b" }
-variable "clusterSubnet"                { default = "subnet-6dc24051" }
-variable "clusterNodeSecurityGroup"     { default = "sg-dc56e5a0" }
-variable "nodeRole"                     { default = "nodes.solid-telegram.api.zvelo.com" }
+variable "clusterSubnet"                { default = "" }
+variable "clusterNodeSecurityGroup"     { default = "" }
+variable "nodeRole"                     { default = "" }
 variable "clusterName"                  { default = "solid-telegram" }
 variable "region"                       { default = "us-east-1" }
 
